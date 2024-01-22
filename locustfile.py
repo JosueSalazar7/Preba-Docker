5926c5c984014423cf3d6fc3afc4766a544fb134
JosueSalazar7 <josue.salazar@epn.edu.ec>
2024-01-22 15:38:42 -0500

    locust

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,7 @@
-from locust import HttpUser, task, beween
+from locust import HttpUser, task, between
 
 class MyUser(HttpUser):
-    wait_time= beween(1,2)
+    wait_time= between(1,2)
 
     @task
     def index(self):
