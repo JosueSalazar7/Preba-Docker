ea3c15a3d7101ce006487f46289675bd3a4e3a1c
JosueSalazar7 <josue.salazar@epn.edu.ec>
2024-01-22 14:35:06 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import flask
+
+servidor = Flask(__name__)
+
+@servidor.route('/')
+def hola():
+    return "Hola desde servidor"
+
+if __name__ == '__main__':
+    servidor.run(host='0.0.0.0')
